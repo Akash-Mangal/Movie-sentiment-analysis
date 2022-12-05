4c89b40924cdeddaacb18766e4648a9de3f469dc
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2022-12-05 21:34:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ choice=st.cache()
 with st.container():
     st.write('<style>body { font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 800px;z-index: 1; background: White; color: #F63366; position:fixed;top:20px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Movie Sentiment Analysis"+'</b></h2></div>', unsafe_allow_html=True)
 with st.container():
-   
-img_col=st.columns((4,2,3))
+   img_col=st.columns((4,2,3))
     img_col[0].image('download.jpg',use_column_width=True)
     #img_col[0].write(ABOUT)
 
