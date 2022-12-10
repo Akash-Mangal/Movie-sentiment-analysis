c2de2bef4e753185e40300d14c01411a9930f204
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2022-12-10 23:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,10 @@ review= ['Dislike','Like']
 
 labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ax1.pie(cnt, explode=explode, labels=review, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
