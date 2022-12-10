7bc7d723de808f2a58596502a1650955ce403f95
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2022-12-10 07:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ with st.container():
    for i in coment:
      img_col[1].write(i)
 
-f="This is a good movie to watch, amazing and have a lots of fun"
+#f="This is a good movie to watch, amazing and have a lots of fun"
 inp=clean(f)
    
 with open("cv1.pkl",'rb') as f1:
