def7e5ad1c49b4bfcb67e77635656f04d95153d9
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2022-12-10 08:14:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 
 coment=["Excellent Sequel ,Outstanding edge of the seat thriller , poora paisa vasool 4 Star ⭐️⭐️⭐️⭐️Firstly Big big big salute to Jeetu Joseph for this masterpiece original story for Drishyam 2 . Vijay Salgaonkar returns in the 2nd installment standing strong to save his family yet again , the film is shot in Goa , there are beautiful locales , the thrill factor drives the film ,the narration & screenplay just flows organically and you just get engaged & enjoy the whole story telling ,such crisp is the screenplay . ",
         " haven’t seen Malayalam version of this movie so I won’t be comparing with it. This movie has amazing plot with some really outstanding twists and turns.  Ajay Devgn, well no one can beat him in this style of acting, his eyes and his body language are dialogues for him. New entrant in this sequel - Akshaye Khanna was outstanding and probably he only could have taken up this role",
-       "",
-        "",
+       "I don’t think I need to go into too much detail as this is a direct continuation of the first one and it’s very very good! \nI actually didn’t think the first one was THAT great but on a strong recommendation me and hubby watched it at home, not thinking about not too much it I waited for the second instalment and I wasn’t disappointed! ",
+        "I watched the movie in theaters two days ago. It was a great experience. A classic thriller movie that hits bang on the emotion of going to an extent to save the family. \nI watched Drishyam on my Television and I was blown away with the suspense till last scene and when I got to know that Part 2 is releasing I decided that day to watch it in theaters, just for the larger than life screen experience. 
+",
         "",
         "",
        
