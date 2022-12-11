6848b674668da8dfcef733a31510d47b71537c2c
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2022-12-11 23:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,10 @@ colors=['#3b5999','#FF577F']
 for i in coment:
     if pred[a]==0:
         a+=1
-        img_col[1].markdown("<p style='text-align: justify; color: #3b5999;'>"+i+"</p>", unsafe_allow_html=True)  
+        img_col[1].markdown("<p style='text-align: justify; color: #3b5999;'><b>"+i+"</b></p>", unsafe_allow_html=True)  
     else:
        a+=1
-       img_col[1].markdown("<p style='text-align: justify; color: #F63366;'>"+i+"</p>", unsafe_allow_html=True) 
+       img_col[1].markdown("<p style='text-align: justify; color: #F63366;'><b>"+i+"</b></p>", unsafe_allow_html=True) 
    
 
 fig1, ax1 = plt.subplots()
