8bac71a6fe9f8e620d2cd4d4135c293911bdd24d
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2025-09-04 15:05:33 +0530

    Update app.py
    st.cache was depricated needs an update to st.cache_data

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def clean(x):
     return x.lower()
 
 st.set_page_config(layout="wide")
-choice=st.cache()
+choice=st.cache_data()
 #st.title(PROJECT_NAME)
 with st.container():
     st.write('<style>body .sticky{ font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 800px;z-index: 1; background: White; color: #F63366; position:fixed;top:20px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Movie Sentiment Analysis"+'</b></h2></div>', unsafe_allow_html=True)
@@ -80,4 +80,5 @@ ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 img_col[2].pyplot(fig1)
 img_col[2].markdown("<p style='text-align: center; color: #FF577F;'><b> Like: "+str(one)+"</b></p><p style='text-align: center; color: #3b5999;'><b> Dislike: "+str(ze)+"</b></p></div>", unsafe_allow_html=True)
+
 
